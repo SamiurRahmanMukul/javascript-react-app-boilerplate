90c09c7ce1dc8f3ccb91c6888421c85d97ccde17
Md. Samiur Rahman Mukul <97078249+Samiur-SiliconOrchard@users.noreply.github.com>
2022-08-17 09:23:01 +0600

    react-router-dome@v6 using routing setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className='min-h-screen bg-gray-gradient'>
-      <h1 className='text-center text-2xl text-gradient font-bold pt-5'>
-        Welcome! JavaScript React App Boilerplate !!!
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Home() {
+  window.document.title = 'React App — Home';
+
+  return (
+    <div className='min-h-screen bg-gray-gradient'>
+      <h1 className='text-center text-2xl text-gradient font-bold pt-5'>
+        Welcome! JavaScript React App Boilerplate !!!
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
